3ff81b18c697b800fbf81624b2ea09e790830a43
Rafael Rodriguez <rafael.nc@me.com>
2024-04-29 12:24:49 -0400

    Learn Swift UI Version 0.1

--- Gallaugher/ContentView.swift
+++ Gallaugher/ContentView.swift
@@ -8,12 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messageString = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                  
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundColor(.red)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            HStack {
+                
+                Button("Awesome") {
+                    // This is the action performed when the button is pressed.
+                    messageString = "You Are Awesome!"
+                }
+                .buttonStyle(.borderedProminent)
+                
+                Button("Great") {
+                    // This is the action performed when the button is pressed.
+                    messageString = "You Are Great!"
+                }
+                .buttonStyle(.borderedProminent)
+            }
+            
         }
         .padding()
     }
