37f308f61fa988d1782c054c45803aa67bcbbc9e
Rafael Rodriguez <rafael.nc@me.com>
2024-04-29 10:42:51 -0400

    Initial Commit

---
+++ Gallaugher/GallaugherApp.swift
@@ -0,0 +1,17 @@
+//
+//  GallaugherApp.swift
+//  Gallaugher
+//
+//  Created by Rafael Rodriguez on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct GallaugherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
