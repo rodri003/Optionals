f2f56d8dc75949edcb6273c5d551fcba4564bbff
Rafael Rodriguez <rafael.nc@me.com>
2024-04-29 18:07:13 -0400

    Ch 1.8 Images with multiple colors, overlay

--- Gallaugher/ContentView.swift
+++ Gallaugher/ContentView.swift
@@ -8,12 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var messageString = ""
+    @State private var messageString = "Namaste"
     
     var body: some View {
         GeometryReader {geometry in
             VStack {
                 Spacer()
+                
+                Image(systemName: "cloud.sun.rain.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .symbolRenderingMode(.multicolor)
+                    .background(Color(hue: 0.521, saturation: 0.299, brightness: 0.958))
+                    .cornerRadius(30)
+                    .shadow(color: .gray, radius: 30, x: 20, y: 20)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 30)
+                            .stroke(.teal, lineWidth: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
+                    )
+                   
+                    .padding()
+                
                 
                 Text(messageString)
                     .font(.largeTitle)
@@ -38,12 +53,15 @@ struct ContentView: View {
                     }
                     .buttonStyle(.borderedProminent)
                    
+                    Spacer()
+                    
                     Button("Great") {
                         // This is the action performed when the button is pressed.
                         messageString = "You Are Great!"
                     }
                     .buttonStyle(.borderedProminent)
                 }
+                .padding()
                // Spacer()
             }
         }
