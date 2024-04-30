536a1df8c52e25519660d42932344179b06e07cb
Rafael Rodriguez <rafael.nc@me.com>
2024-04-30 17:04:36 -0400

    Ch. 1.11 Adding your own Images

--- Gallaugher/ContentView.swift
+++ Gallaugher/ContentView.swift
@@ -8,25 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var messageString = ""
+    @State private var messageName = ""
+    @State private var imageName = ""
     
     var body: some View {
         GeometryReader {geometry in
             VStack {
+                Image(imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .cornerRadius(5)
+                    .padding()
                 Spacer()
                 
                 
                 
                 
                 
-                Text (messageString)
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    .minimumScaleFactor(0.5)
-                    .foregroundColor(.red)
-                    .multilineTextAlignment(.center)
-                    .frame(height: 150)
-                    .frame(maxWidth: .infinity)
+//                Text (messageString)
+//                    .font(.largeTitle)
+//                    .fontWeight(.heavy)
+//                    .minimumScaleFactor(0.5)
+//                    .foregroundColor(.red)
+//                    .multilineTextAlignment(.center)
+//                    .frame(height: 150)
+//                    .frame(maxWidth: .infinity)
                 //   .border(.orange, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
                     .padding()
                 
@@ -37,14 +43,14 @@ struct ContentView: View {
                 
                 Button("Show Message") {
                     // This is the action performed when the button is pressed.
-                    let message1 = "You are Awesome!"
-                    let message2 = "You are Great!"
+                    let image1 = "horizontal"
+                    let image2 = "vertical"
                     //                    if messageString == message1 {
                     //                        messageString = message2
                     //                    } else {
                     //                        messageString = message1
                     //                    }
-                    messageString = (messageString == message1 ? message2 : message1)
+                    imageName = (imageName == image1 ? image2 : image1)
                 }
                 .buttonStyle(.borderedProminent)
                 
