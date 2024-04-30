905243a9eef55edf877fbdf283ddcf5e3967eeee
Rafael Rodriguez <rafael.nc@me.com>
2024-04-29 21:55:16 -0400

    Ch. 1.9 Swift Conditionals

--- Conditional.playground/Contents.swift
+++ Conditional.playground/Contents.swift
@@ -1,3 +1,19 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var messageString = "I am a Developer!"
+//print("Hello There!" , messageString) //adds a space
+//print("Hello There!" + messageString) // concatanates strings
+//print ("Hello There! \(messageString)") // String Interpolation
+
+var person = "Rafael"
+
+if person == "Limor" {
+    print ("Hello, Lady Ada!")
+} else if person == "Grace" {
+    print ("Hello, Admiral Hopper!")
+} else {
+    print ("Hello There, \(person)!")
+}
+
+let pi = 3.14
+
--- Gallaugher/ContentView.swift
+++ Gallaugher/ContentView.swift
@@ -8,29 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var messageString = "Namaste"
+    @State private var messageString = ""
     
     var body: some View {
         GeometryReader {geometry in
             VStack {
                 Spacer()
                 
-                Image(systemName: "cloud.sun.rain.fill")
-                    .resizable()
-                    .scaledToFit()
-                    .symbolRenderingMode(.multicolor)
-                    .background(Color(hue: 0.521, saturation: 0.299, brightness: 0.958))
-                    .cornerRadius(30)
-                    .shadow(color: .gray, radius: 30, x: 20, y: 20)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 30)
-                            .stroke(.teal, lineWidth: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
-                    )
-                   
-                    .padding()
                 
                 
-                Text(messageString)
+                
+                
+                Text (messageString)
                     .font(.largeTitle)
                     .fontWeight(.heavy)
                     .minimumScaleFactor(0.5)
@@ -38,31 +27,31 @@ struct ContentView: View {
                     .multilineTextAlignment(.center)
                     .frame(height: 150)
                     .frame(maxWidth: .infinity)
-                 //   .border(.orange, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                //   .border(.orange, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
                     .padding()
                 
                 Spacer()
                 
-               
                 
-                HStack {
-                    
-                    Button("Awesome") {
-                        // This is the action performed when the button is pressed.
-                        messageString = "You Are Awesome!"
+                
+                
+                Button("Show Message") {
+                    // This is the action performed when the button is pressed.
+                    let message1 = "You are Awesome!"
+                    let message2 = "You are Great!"
+                    if messageString == message1 {
+                        messageString = message2
+                    } else {
+                        messageString = message1
                     }
-                    .buttonStyle(.borderedProminent)
-                   
-                    Spacer()
-                    
-                    Button("Great") {
-                        // This is the action performed when the button is pressed.
-                        messageString = "You Are Great!"
-                    }
-                    .buttonStyle(.borderedProminent)
                 }
+                .buttonStyle(.borderedProminent)
+                
+                
+                
+                
                 .padding()
-               // Spacer()
+                // Spacer()
             }
         }
         //.padding()
