fc490905e905fa59b4e61041ad3b91260e71052e
Rafael Rodriguez <rafael.nc@me.com>
2024-05-06 18:00:35 -0400

    Ch 1.22 Hide Label to allow toggle to left-aligned, .onChange, Compound Conditional

--- Gallaugher/ContentView.swift
+++ Gallaugher/ContentView.swift
@@ -17,6 +17,7 @@ struct ContentView: View {
     @State private var lastMessageNumber = -1
     @State private var audioPlayer: AVAudioPlayer!
     @State private var lastSoundNumber = -1
+    @State private var soundIsOn = true
     
     var body: some View {
         GeometryReader {geometry in
@@ -42,83 +43,116 @@ struct ContentView: View {
                 Spacer()
                 
                 
-                Button("Show Message") {
+                HStack {
+                 
+                    Text ("Toggle is On:")
+                        
+                       
+                    Toggle("", isOn: $soundIsOn)
+                        .labelsHidden()
+//                        .onChange(of: soundIsOn) { _, _ in
+//                            if audioPlayer != nil {
+//                                if audioPlayer.isPlaying {
+//                                    audioPlayer.stop()
+//                                }
+//                            }
+//                        }
+                    // Modified with a compound conditional
+                        .onChange(of: soundIsOn) { _, _ in
+                            if audioPlayer != nil && audioPlayer.isPlaying {
+                                audioPlayer.stop()
+                            }
+                        }
+                                
+                      Spacer()
+                       
                     
-                    let messages = ["You are Awesome!",
-                                    "You Are Great!" ,
-                                    "Fabulous? That's You!",
-                                    "You are Fantastic",
-                                    "You Swifty!",
-                                    "You Are a Code Monster",
-                                    "You Did It!"]
-                    
-                    var messageNumber: Int
-                                        
-                    repeat {
-                        messageNumber = Int.random(in: 0...messages.count - 1)
-                    } while messageNumber == lastMessageNumber
-                    messageString = messages[messageNumber]
-                    lastMessageNumber = messageNumber
-                    
-                    
-                    
-                    //                    messageString = messages[messageNumber]
-                    //                    messageNumber += 1
-                    //                    if messageNumber == messages.count {
-                    //                        messageNumber = 0
-                    //                    }
-                    
-                    
-                    var imageNumber: Int
-                    
-                    repeat {
-                        imageNumber = Int.random(in: 0...9)
-                    } while imageNumber == lastImageNumber
-                    imageName = "image\(imageNumber)"
-                    lastImageNumber = imageNumber
-                                       
-                    
-                    //TODO:  - update the imageName - variable
-                    //                    imageName = "image\(imageNumber)"
-                    //
-                    //                    imageNumber += 1
-                    //                    if imageNumber > 9 {
-                    //                        imageNumber = 0
-                    //                    }
-                    //                    print (imageNumber)
-                    
-                    // Challenge random sounds non-repeating
-                    
-                    var soundNumber: Int
-                    
-                    repeat {
-                        soundNumber = Int.random(in: 0...5)
-                    } while soundNumber == lastSoundNumber
-                    let soundName = "sound\(soundNumber)"
-                   
-                    guard let soundFile = NSDataAsset(name: soundName) else {
-                        print ("😡 Could not read file named \(soundName)")
-                        return
+
+                    Button("Show Message") {
+                        
+                        let messages = ["You are Awesome!",
+                                        "You Are Great!" ,
+                                        "Fabulous? That's You!",
+                                        "You are Fantastic",
+                                        "You Swifty!",
+                                        "You Are a Code Monster",
+                                        "You Did It!"]
+                            
+                        var messageNumber: Int
+                                            
+                        repeat {
+                            messageNumber = Int.random(in: 0...messages.count - 1)
+                        } while messageNumber == lastMessageNumber
+                        messageString = messages[messageNumber]
+                        lastMessageNumber = messageNumber
+                           
+                        
+                        
+                        //                    messageString = messages[messageNumber]
+                        //                    messageNumber += 1
+                        //                    if messageNumber == messages.count {
+                        //                        messageNumber = 0
+                        //                    }
+                        
+                        
+                        var imageNumber: Int
+                        
+                        repeat {
+                            imageNumber = Int.random(in: 0...9)
+                        } while imageNumber == lastImageNumber
+                        imageName = "image\(imageNumber)"
+                        lastImageNumber = imageNumber
+                                           
+                        
+                        //TODO:  - update the imageName - variable
+                        //                    imageName = "image\(imageNumber)"
+                        //
+                        //                    imageNumber += 1
+                        //                    if imageNumber > 9 {
+                        //                        imageNumber = 0
+                        //                    }
+                        //                    print (imageNumber)
+                        
+                        // Challenge random sounds non-repeating
+                        
+                        // Check to see if Sound toggle is On
+                        
+                        if soundIsOn {
+                            var soundNumber: Int
+                            
+                            repeat {
+                                soundNumber = Int.random(in: 0...5)
+                            } while soundNumber == lastSoundNumber
+                            let soundName = "sound\(soundNumber)"
+                           
+                            guard let soundFile = NSDataAsset(name: soundName) else {
+                                print ("😡 Could not read file named \(soundName)")
+                                return
+                            }
+                            
+                            do {
+                                
+                               audioPlayer =  try AVAudioPlayer(data: soundFile.data)
+                                audioPlayer.play()
+                            } catch {
+                                print ("😡 ERROR: \(error.localizedDescription) creating audioPlayer")
+                                
+                            }
+                        }
+                        
+                        
+                            
+               
+                        
                     }
                     
-                    do {
-                        
-                       audioPlayer =  try AVAudioPlayer(data: soundFile.data)
-                        audioPlayer.play()
-                    } catch {
-                        print ("😡 ERROR: \(error.localizedDescription) creating audioPlayer")
-                        
-                    }
+                    .buttonStyle(.borderedProminent)
                     
-           
                     
+                    
+                    
+        
                 }
-                
-                .buttonStyle(.borderedProminent)
-                
-                
-                
-                
                 .padding()
                 // Spacer()
             }
